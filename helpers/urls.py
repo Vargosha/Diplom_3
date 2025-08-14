8b8165a1912a393346641301ae2fd998214fef3c
Egor <egor.vargo@gmail.com>
2025-08-14 11:27:30 +0300

    Refactor helpers/urls

--- helpers/urls.py
+++ helpers/urls.py
@@ -1,3 +1,5 @@
-HOME_PAGE_URL = "https://stellarburgers.nomoreparties.site/"
-ORDER_FEED_PAGE_URL = "https://stellarburgers.nomoreparties.site/feed"
-LOGIN_PAGE_URL = "https://stellarburgers.nomoreparties.site/login"
+BASE_URL = "https://stellarburgers.nomoreparties.site"
+
+HOME_PAGE_URL = f"{BASE_URL}/"
+ORDER_FEED_PAGE_URL = f"{BASE_URL}/feed"
+LOGIN_PAGE_URL = f"{BASE_URL}/login"
